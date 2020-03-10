0215dd721a8d9965f43b8cf0cc187737a07c432d
barnum87 <62035289+barnum87@users.noreply.github.com>
2020-03-10 17:11:50 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 // TODO: Enter the path to your service account json file
 // Need help with this step go here: https://firebase.google.com/docs/admin/setup
-// const serviceAccount = require("./REPLACE_WITH_SERVICE_ACCOUNT.json");
+// const serviceAccount = require("madden20-b6290-firebase-adminsdk-3i9r6-592c0049e8.json");
 
 // TODO: Enter your database url from firebase
 // admin.initializeApp({
